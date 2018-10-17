7fb5f013e9aa77ab9154ed9bc5b014248b34d743
Arpik Sargsyan <arpi@xamlab.com>
2018-10-17 13:36:18 +0400

    Added setSegmentColor()

--- ColorSlider/ColorSlider/ViewController.swift
+++ ColorSlider/ColorSlider/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenColorField: UITextField!
     @IBOutlet weak var bleueColorField: UITextField!
     @IBOutlet weak var colorSegment: UISegmentedControl!
-    @IBOutlet weak var `switch`: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +24,21 @@ class ViewController: UIViewController {
         redColorSlider.value = 255
         greenColorSlider.value = 0
         blueColorSlider.value = 0    }
-
+    
+    @IBAction func switchValueChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            colorSegment.isEnabled = true
+            redColorSlider.isEnabled = true
+            greenColorSlider.isEnabled = true
+            blueColorSlider.isEnabled = true
+        } else {
+            colorSegment.isEnabled = false
+            redColorSlider.isEnabled = false
+            greenColorSlider.isEnabled = false
+            blueColorSlider.isEnabled = false
+        }
+    }
+    
     @IBAction func segmentValueChanged(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             view.backgroundColor = UIColor.red
@@ -49,26 +62,45 @@ class ViewController: UIViewController {
         blueSliderChanged()
         redSliderChanged()
         greenSliderChanged()
+        
+    }
+    
+    @IBAction func redSliderChanged() {
+        redColorField.text = String(Int(redColorSlider.value))
+        setBackgroundColor()
+        setCorrectSegmentValue()
+        setSegmentColor()
+    }
+    
+    @IBAction func greenSliderChanged() {
+        greenColorField.text = String(Int(greenColorSlider.value))
+        setBackgroundColor()
+        setCorrectSegmentValue()
+        setSegmentColor()
+    }
+    
+    @IBAction func blueSliderChanged(){
+        bleueColorField.text = String(Int(blueColorSlider.value))
+        setBackgroundColor()
+        setCorrectSegmentValue()
+        setSegmentColor()
     }
     
     func setCorrectSegmentValue() {
         if redColorField.text == String(255) &&
             greenColorField.text == String(0) &&
             bleueColorField.text == String(0) {
-            
-            colorSegment.selectedSegmentIndex = 0
+                colorSegment.selectedSegmentIndex = 0
             
         } else if redColorField.text == String(0) &&
             greenColorField.text == String(255) &&
             bleueColorField.text == String(0) {
-            
-            colorSegment.selectedSegmentIndex = 1
+                colorSegment.selectedSegmentIndex = 1
             
         } else if redColorField.text == String(0) &&
             greenColorField.text == String(0) &&
             bleueColorField.text == String(255) {
-            
-            colorSegment.selectedSegmentIndex = 2
+                colorSegment.selectedSegmentIndex = 2
         }
     }
     
@@ -79,22 +111,17 @@ class ViewController: UIViewController {
         view.backgroundColor = color
     }
     
-    @IBAction func redSliderChanged() {
-        redColorField.text = String(Int(redColorSlider.value))
-        setBackgroundColor()
-        setCorrectSegmentValue()
-    }
-    
-    @IBAction func greenSliderChanged() {
-        greenColorField.text = String(Int(greenColorSlider.value))
-        setBackgroundColor()
-        setCorrectSegmentValue()
-    }
-    
-    @IBAction func blueSliderChanged(){
-        bleueColorField.text = String(Int(blueColorSlider.value))
-        setBackgroundColor()
-        setCorrectSegmentValue()
+    func setSegmentColor() {
+        if  redColorField.text == String(0) &&
+            greenColorField.text == String(0) &&
+            bleueColorField.text == String (0) {
+                colorSegment.tintColor = UIColor.white
+            
+        } else if redColorField.text == String(255) &&
+                  greenColorField.text == String(255) &&
+                  bleueColorField.text == String (255) {
+                    colorSegment.tintColor = UIColor.black
+        }
     }
 }
 
